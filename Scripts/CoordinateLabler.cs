4bf67ab9610a4749fc15440a08641fcfdc92dff1
Josh O <72672143+Weunbeatable@users.noreply.github.com>
2021-09-28 00:58:51 -0400

    Add files via upload

---
+++ Scripts/CoordinateLabler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[ExecuteAlways]
+[RequireComponent(typeof(TextMeshPro))]
+public class CoordinateLabler : MonoBehaviour
+{
+    //use the access of is placeable to change coordinate colour
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color blockColor = Color.clear;
+    //**********************************************************
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+    waypoint waypoint; // access to our waypoints
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+        waypoint = GetComponentInParent<waypoint>(); // we get component in parent because the waypoint is on the root of our object and the coordinate is on the root of the children.
+        DisplayCoordinaties();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinaties();
+            UpdateObjectName();
+            //do something
+        }
+        SetLabelColor();
+        ToggleLabels();
+    }
+
+    void ToggleLabels()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive(); // toggling the visibility of our label.
+        }
+       
+    }
+    private void SetLabelColor()
+    {
+        if (waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+            label.color = blockColor;
+    }
+
+    void DisplayCoordinaties()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+    
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
---
+++ Scripts/waypoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoint : MonoBehaviour
+{
+    [SerializeField] Tower towerPrefab;
+
+    [SerializeField] bool isPlaceable; //check if we can place a tower on that spot
+   
+    public bool IsPlaceable  { get { return isPlaceable; } }  // Going to use a propery, Note the format isplaceable{} not is paceable(){}
+    private void OnMouseDown()
+    {
+        if (isPlaceable) 
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;//This will prevent us from placing the same tile  mulitple times on the same location.
+
+        }
+    }
+
+}
